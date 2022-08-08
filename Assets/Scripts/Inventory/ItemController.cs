3750ea879b628b30df47aaa3e1fbf5c46763cb5a
Mario Gabriel Baravaglio <maritobaravaglio@gmail.com>
2022-08-08 13:16:10 -0300

    Revert "Add Locomotion and Roll"
    This reverts commit f1ea067f1520cc7d29e50662641a416ae39f3967.

--- Assets/Scripts/Inventory/ItemController.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ItemController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    public Item Item;
-}
